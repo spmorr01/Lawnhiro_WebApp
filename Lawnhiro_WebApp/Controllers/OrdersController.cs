eb5676d0f52e50b2e941a7b2e0cdc26afe080e61
spmorr01 <spmorr01@wsc.edu>
2017-01-23 12:23:25 -0600

    commit

---
+++ Lawnhiro_WebApp/Controllers/OrdersController.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using Lawnhiro_WebApp.Models;
+
+namespace Lawnhiro_WebApp.Controllers
+{
+    public class OrdersController : ApiController
+    {
+        Orders[] orders = new Orders[] //add a comment
+        {
+            new Orders {OrderID = 1, Address = "Test Address", Email = "mail@mail.com", PayPalOrderID = "PAY123" },
+            new Orders {OrderID = 2, Address = "Test Address2", Email = "mai2l@mail.com", PayPalOrderID = "PAY1234" },
+            new Orders {OrderID = 3, Address = "Test Address3", Email = "mail3@mail.com", PayPalOrderID = "PAY12345" }
+        };
+
+        // GET api/<controller>
+        public IEnumerable<Orders> GetAllOrders()
+        {
+            return orders;
+        }
+
+        // GET api/<controller>/5
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST api/<controller>
+        public void Post([FromBody]string value)
+        {
+        }
+
+        // PUT api/<controller>/5
+        public void Put(int id, [FromBody]string value)
+        {
+        }
+
+        // DELETE api/<controller>/5
+        public void Delete(int id)
+        {
+        }
+    }
+}
---
+++ Lawnhiro_WebApp/Models/Orders.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Lawnhiro_WebApp.Models
+{
+    public class Orders
+    {
+        public int OrderID { get; set; }
+        public string Address { get; set; }
+        public string Email { get; set; }
+        public string PayPalOrderID { get; set; }
+    }
+}
