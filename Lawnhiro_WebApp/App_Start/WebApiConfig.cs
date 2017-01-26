6f80158d64da9e5dc5e658f1321b245fec4a36bb
spmorr01 <spmorr01@wsc.edu>
2017-01-25 22:34:05 -0600

    Added active jobs page with GET data from web api.

--- Lawnhiro_WebApp/App_Start/WebApiConfig.cs
+++ Lawnhiro_WebApp/App_Start/WebApiConfig.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Http.Headers;
 using System.Web.Http;
 
 namespace Lawnhiro_WebApp
@@ -19,6 +20,9 @@ namespace Lawnhiro_WebApp
                 routeTemplate: "api/{controller}/{id}",
                 defaults: new { id = RouteParameter.Optional }
             );
+
+            config.Formatters.JsonFormatter.SupportedMediaTypes
+                .Add(new MediaTypeHeaderValue("text/html"));
         }
     }
 }
---
+++ Lawnhiro_WebApp/Scripts/ActiveJobs.js
@@ -0,0 +1,37 @@
+﻿
+
+var activeOrderVm = {
+    activeOrders: ko.observableArray()
+}
+
+    function initialize() {
+        getActiveOrders();
+        ko.applyBindings(activeOrderVm, document.getElementById("HeaderDiv"));
+    }
+
+    function claimJob(orderId) {
+        alert("You claimed order number: " + orderId);
+    }
+
+    function getActiveOrders() {
+        $.ajax({
+            url: '/api/Orders',
+            type: 'GET',
+            dataType: 'json',
+            contentType: 'application/json; charset=utf-8',
+            
+            success: function (data) {
+                //alert(data);
+                //showProducts(data);
+                //$.each(data, function (item) {
+                //    alert(item.PageName);
+                //});
+                activeOrderVm.activeOrders(data);
+            },
+            fail: function (jqXHR, textStatus) {
+                alert("Request failed: " + textStatus);
+            }
+        })
+    }
+
+
